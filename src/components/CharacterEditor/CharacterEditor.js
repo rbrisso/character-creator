a5c824a7ab6ae9883704002cddd23f2029a884d8
Rodrigo Brisso <rbrisso@Rodrigos-MacBook-Air.local>
2022-08-24 22:33:15 -0400

    solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -26,10 +26,11 @@ function App() {
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
   const [clothesColor, setClothesColor] = React.useState(
     defaultClothesColor
-  );
-
-  return (
-    <main className={styles.characterEditor}>
+    );
+    
+    return (
+      <main className={styles.characterEditor}>
+      <div className={styles.perspective} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
